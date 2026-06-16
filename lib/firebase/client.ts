f9b59ba624dacef8f70f17b8f7376ede95693d1c
tue-truong-official <tue.truonga@one-line.com>
2026-06-16 22:34:17 +0700

    feat: Refactor authDomain assignment to use dynamic function for improved flexibility

--- lib/firebase/client.ts
+++ lib/firebase/client.ts
@@ -2,9 +2,24 @@ import { FirebaseApp, getApp, getApps, initializeApp } from "firebase/app";
 import { Auth, getAuth } from "firebase/auth";
 import { Firestore, getFirestore } from "firebase/firestore";
 
+function getFirebaseAuthDomain() {
+  const authDomain = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+  const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+
+  if (authDomain) {
+    return authDomain;
+  }
+
+  if (projectId) {
+    return `${projectId}.firebaseapp.com`;
+  }
+
+  return undefined;
+}
+
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  authDomain: getFirebaseAuthDomain(),
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
